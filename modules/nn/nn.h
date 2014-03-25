6b2ac16290daae2f9409073152d289e55c09cf19
jungle-cat <ustcrevolutionary@gmail.com>
2014-03-25 10:49:13 +0800

    init

---
+++ modules/nn/nn.h
@@ -0,0 +1,18 @@
+/*
+ * nn.h
+ *
+ *  Created on: 2014-3-25
+ *      Author: Feng
+ */
+
+#ifndef __NN_H__
+#define __NN_H__
+
+namespace dl {
+
+
+
+
+}
+
+#endif /* __NN_H__ */
---
+++ modules/optimizer/optimizer.h
@@ -0,0 +1,26 @@
+/*
+ * optimizer.h
+ *
+ *  Created on: 2014-3-25
+ *      Author: Feng
+ */
+
+#ifndef __OPTIMIZER_H__
+#define __OPTIMIZER_H__
+
+#include <opencv/cv.h>
+
+namespace dl{
+
+class Optimizer
+{
+	public:
+	private:
+};
+
+
+}
+
+
+
+#endif /* __OPTIMIZER_H__ */
